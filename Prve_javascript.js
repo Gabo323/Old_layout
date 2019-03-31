ea57fe6865182802e0c938a39267f6c437f48234
Gabriel Lengyel <lengabriel32@gmail.com>
2019-03-31 20:50:18 +0200

    Posting old layout

---
+++ Prve_javascript.js
@@ -0,0 +1,35 @@
+﻿function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "inline") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "inline";
+    }
+}
+
+var i = 0;
+var images = [];
+var time = 3000;
+
+
+images[0] = "Fotky/Photo.jpg";
+images[1] = "Fotky/Photo_2.jpg";
+images[2] = "Fotky/Photo.jpg";
+images[3] = "Fotky/Photo_2.jpg";
+
+
+function changeImg() {
+    document.slide.src = images[i];
+    if (i < images.length - 1) {
+        i++;
+    }
+    else {
+        i = 0;
+    }
+
+
+    setTimeout("changeImg()", time);
+}
+
+
+window.onload = changeImg;
